89824611eabcf39652e3290cfbbde998f741fba1
RIzqi Rafly <140831940+Rizqirafly@users.noreply.github.com>
2023-09-12 08:46:00 +0700

    Add files via upload

---
+++ lat2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="proses.php" method="post">
+        Nilai : <input type="number" name="nilai"><br>
+        Absen : <input type="number" name="absen"><br>
+        <input type="submit" value="PROSES" name="submit">
+    </form>
+</body>
+</html>
